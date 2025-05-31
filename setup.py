2c2604d906cfc979fc9c7e8cf929d78c6d845bdb
Arpita Mallik <arpitamallik13@gmail.com>
2025-05-31 21:12:02 +0600

    project structure ready

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Arpita Mallik",
+    author_email="arpitamallik13@gmail.com",
+    packages=find_packages()
+)
